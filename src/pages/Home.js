7ab86550b2930c7ea943c15344e14c230d6642d5
thanhnguyen <thanhnguyentuan000@gmail.com>
2022-05-07 10:17:52 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../assets/css/MusicList.css"
 
 function Home() {
   return (
     <div className="home">
-      <Link to={"/nhac-tre"} className="linknhac">Nhac tre</Link>
-      <Link to={"/rapviet"} className="linknhac">Rap Viet</Link>
-      <Link to={"/nhac-trinh"} className="linknhac">Nhac trinh</Link>
-      <Link to={"/pop"} className="linknhac">Nhac Pop</Link>
-      <Link to={"/E-D"} className="linknhac">Nhac electronic</Link>
-      <Link to={"/Rap"} className="linknhac">Nhac Rap</Link>
-      <Link to={"/Jazz"} className="linknhac">Nhac Jazz</Link>
-      <Link to={"/Coutry"} className="linknhac">Nhac Coutry</Link>
-      <Link to={"/latin"} className="linknhac">Nhac Latin</Link>
-      <Link to={"/japan"} className="linknhac">Nhac Japan</Link>
-      <Link to={"/korea"} className="linknhac">Nhac Korea</Link>
-      <Link to={"/koloi"} className="linknhac">Nhac KO loi</Link>
+      <Link to={"/nhac-tre"} className="linknhac nhactre">Nhac tre</Link>
+      <Link to={"/rapviet"} className="linknhac rapviet">Rap Viet</Link>
+      <Link to={"/nhac-trinh"} className="linknhac nhactrinh">Nhac trinh</Link>
+      <Link to={"/pop"} className="linknhac pop">Nhac Pop</Link>
+      <Link to={"/E-D"} className="linknhac elec">Nhac electronic</Link>
+      <Link to={"/Rap"} className="linknhac rap">Nhac Rap</Link>
+      <Link to={"/Jazz"} className="linknhac jazz">Nhac Jazz</Link>
+      <Link to={"/Coutry"} className="linknhac coutry">Nhac Coutry</Link>
+      <Link to={"/latin"} className="linknhac latin">Nhac Latin</Link>
+      <Link to={"/japan"} className="linknhac japan">Nhac Japan</Link>
+      <Link to={"/korea"} className="linknhac korea">Nhac Korea</Link>
+      <Link to={"/koloi"} className="linknhac koloi">Nhac KO loi</Link>
     </div>
   );
 }
